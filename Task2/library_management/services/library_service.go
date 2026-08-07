7401a743b6bf79639196d1a213027a37b0081419
MagMc007 <mergashasho7@gmail.com>
2026-08-07 10:28:21 +0300

    completed the methods

--- Task2/library_management/services/library_service.go
+++ Task2/library_management/services/library_service.go
@@ -1,13 +1,132 @@
-package main
+package services
+import (
+	"library_management/models"
+	"errors"
+)
 
 type LibraryManager interface {
-	AddBook(book Book)
+	AddBook(book models.Book)
 	RemoveBook(bookID int)
 
-	BorrowBook(bookID int, memberID int) error
-	ReturnBook(bookID int, memberID int) error
+	BorrowBook(bookID int, memberID int) errors
+	ReturnBook(bookID int, memberID int) errors
 
-	ListAvailableBooks() []Book
-	ListBorrowedBooks(memeberID int) []Book
+	ListAvailableBooks() []models.Book
+	ListBorrowedBooks(memberID int) []models.Book
 }
 
+
+// declare the struct
+type Library struct {
+	bookMap map[int]models.Book{}
+	memberMap map[int]models.Member{}
+}
+
+// method implementations
+func (l *Library) AddBook(book models.Book) {
+	l.bookMap[book.ID] = book
+}
+
+
+func (l *Library) RemoveBook(bookID int) {
+	delete(l.bookMap,bookId)
+}
+
+func (l *Library) BorrowBook(bookID int, memberID int) errors {
+	// is book avalable 
+	book, exists := l.bookMap[bookID]
+
+	if !exists {
+		return errors.New("Book does not exist")
+	}
+
+	// check the book status
+	if book.Status != "Available" {
+		return errors.New("Book has been borrowed")
+	}
+	// does member exist
+	member, exists :=  l.memberMap[memberID]
+
+	if !exists {
+		return errors.New("Member does not exist")
+	}
+
+
+	member.BorrowedBooks = append(member.BorrowedBooks, book)
+
+	book.Status = "Borrowed"
+
+	// update the map with the newly updated memeber and book
+	l.bookMap[bookID] = book
+	l.memberMap[memberID] = member
+
+	return nil
+}
+
+
+func (l *Library) ReturnBook(bookID int, memberID int) errors{
+	// is book avalable 
+	book, exists := l.bookMap[bookID]
+
+	if !exists {
+		return errors.New("Book does not exist")
+	}
+
+	// check the book status
+	if book.Status != "Borrowed" {
+		return errors.New("Book has was not Borrowed")
+	}
+
+	// does member exist
+	member, exists :=  l.memberMap[memberID]
+
+	if !exists {
+		return errors.New("Member does not exist")
+	}
+
+	// make the book availbale
+	book.Status = "Available"
+
+	// remove it from the memebers borrowed slice
+	for i, borrowedBook := range member.BorrowedBooks {
+		if borrowedBook.ID == bookID {
+			member.BorrowedBooks = append(
+				member.BorrowedBooks[:i],
+				member.BorrowedBooks[i+1:]...,
+			)
+			break
+		}
+	}
+
+
+	// update the map
+	l.bookMap[bookID] = book
+	l.memberMap[memberID] = member
+
+	return nil
+} 
+
+func (l *Library) ListAvailableBooks() []models.Book {
+	// prepare an array append each to it from the map
+	books := make([]models.Book, 0)
+
+	for _, book := range l.bookMap {
+		if book.Status == "Available" {
+			books = append(books, book)
+		}
+	}
+
+	return books
+}
+
+func (l *Library) ListBorrowedBooks(memberID int)  []models.Book {
+	books := make([]models.Book, 0)
+	// does the member exist
+	member, exists := l.memberMap[memberID]
+
+	if !exists {
+		return books
+	}
+
+	return member.BorrowedBooks
+}
