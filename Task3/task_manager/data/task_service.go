f386638e3d2e7383ca3978b5d7f2dbe4482269d4
MagMc007 <mergashasho7@gmail.com>
2026-08-08 22:35:57 +0300

    completed models and services

---
+++ Task3/task_manager/data/task_service.go
@@ -0,0 +1,61 @@
+package data
+
+import (
+	"errors"
+
+	"task_manager/models"
+)
+
+// prepare services for CRUD ops
+// prepare an interface
+
+type TaskServices interface {
+	AddTask(task models.Task) models.Task
+	GetAllTasks() []models.Task
+
+	GetTaskDetails(id int) (models.Task, error)
+	UpdateTask(id int, task model.Task) (models.Task, error)
+	DeleteTask(id int) error
+}
+
+// extend each method in the interface and implement
+func (t *[]models.Task) AddTask(task models.Task) models.Task {
+	*t = append(*t, task)
+
+	return task
+}
+
+func (t *[]models.Task) GetAllTasks() []models.Task{
+	return *t
+}
+
+func (t *[]models.Task) GetTaskDetails(id int) (models.Task, error) {
+	for _, v := range *t {
+		if id == v.ID {
+			return (v, nil)
+		}
+	}
+
+	return (models.Task{}, errors.New("Task with this ID does not exist"))
+}
+
+
+func (t *[]models.Task) UpdateTask(id int, task models.Task) (models.Task, error) {
+	for i, v := range *t {
+		if id == v.ID {
+			(*t)[i] = task	
+		}
+	}
+
+	return (models.Task{}, errors.New("Task with this ID does not exist"))	
+}
+
+func (t *[]models.Task) DeleteTask(id int) error {
+	for i, v := range *t {
+		if id == v.ID {
+			(*t) = append((*t)[:i], (*t)[i+1:]...)
+		}
+	}
+
+	return (errors.New("Task with this ID does not exist"))	
+}
