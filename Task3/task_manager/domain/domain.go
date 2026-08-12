efd6fe3529eed01e4eb20818351da6372c3f4d3e
MagMc007 <mergashasho7@gmail.com>
2026-08-12 15:26:21 +0300

    refactor: separated code into a domain layer defined interfaces in the domain

---
+++ Task3/task_manager/domain/domain.go
@@ -0,0 +1,52 @@
+package	domain
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// structs
+type Task struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Title       string             `bson:"title" json:"title"`
+	Description string             `bson:"description" json:"description"`
+	Duedate     time.Time          `bson:"dueDate" json:"dueDate"`
+	Status      string             `bson:"status" json:"status"`
+}
+
+
+type User struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Username string             `bson:"username" json:"username"`
+	Password string             `bson:"password" json:"-"`
+	Role     string             `bson:"role" json:"role"`
+}
+
+// interfaces
+type TaskRepository interface {
+	AddTask(task Task) (Task, error)
+	GetAllTasks() ([]Task, error)
+	GetTaskDetails(id primitive.ObjectID) (Task, error)
+	UpdateTask(id primitive.ObjectID, task Task) (Task, error)
+	DeleteTask(id primitive.ObjectID) error
+}
+
+type TaskUsecase interface {
+	AddTask(task Task) (Task, error)
+	GetAllTasks() ([]Task, error)
+	GetTaskDetails(id primitive.ObjectID) (Task, error)
+	UpdateTask(id primitive.ObjectID, task Task) (Task, error)
+	DeleteTask(id primitive.ObjectID) error
+}
+
+// usecases
+type UserRepository interface {
+	Register(user User) (User, error)
+	Login(username string, password string) (string, error)
+}
+
+type UserUsecase interface {
+    Register(user User) (User, error)
+	Login(username string, password string) (string, error)
+}
