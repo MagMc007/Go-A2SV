689b19e8e1507091db4cc906fb5c474180089380
MagMc007 <mergashasho7@gmail.com>
2026-08-14 14:10:30 +0300

    test: added error tests from usecase functions

--- Task3/task_manager/usecases/task_usecases_test.go
+++ Task3/task_manager/usecases/task_usecases_test.go
@@ -2,6 +2,7 @@ package usecases
 
 import (
 	"testing"
+	"errors"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
@@ -95,6 +96,23 @@ func (suite *TaskUsecaseTestSuite) TestGetTaskDetailsSuccess() {
 	assert.Equal(suite.T(), taskID, result.ID)
 }
 
+func (suite *TaskUsecaseTestSuite) TestGetTaskDetailsError() {
+	taskID := primitive.NewObjectID()
+
+	repositoryErr := errors.New("invalid task ID")
+
+	suite.taskRepository.
+		EXPECT().
+		GetTaskDetails(taskID).
+		Return(domain.Task{}, repositoryErr)
+
+	result, err := suite.taskUsecase.GetTaskDetails(taskID)
+
+	assert.Error(suite.T(), err)
+	assert.Equal(suite.T(), repositoryErr, err)
+	assert.Equal(suite.T(), domain.Task{}, result.ID)
+}
+
 func (suite *TaskUsecaseTestSuite) TestUpdateTaskSuccess() {
 	taskID := primitive.NewObjectID()
 
@@ -122,7 +140,29 @@ func (suite *TaskUsecaseTestSuite) TestUpdateTaskSuccess() {
 	assert.NoError(suite.T(), err)
 	assert.Equal(suite.T(), task, result)
 	assert.Equal(suite.T(), taskID, result.ID)
+}
 
+func (suite *TaskUsecaseTestSuite) TestUpdateTaskError() {
+	taskID := primitive.NewObjectID()
+
+	updatedTask := domain.Task{
+		Title:       "Learn Go Testing",
+		Description: "Learn Testify and Mockery",
+		Status:      "Completed",
+	}
+
+	repositoryErr := errors.New("invalid task ID")
+
+	suite.taskRepository.
+		EXPECT().
+		UpdateTask(taskID, updatedTask).
+		Return(domain.Task{}, repositoryErr)
+
+	result, err := suite.taskUsecase.UpdateTask(taskID, updatedTask)
+
+	assert.Error(suite.T(), err)
+	assert.Equal(suite.T(), repositoryErr, err)
+	assert.Equal(suite.T(), domain.Task{}, result)
 }
 
 func (suite *TaskUsecaseTestSuite) TestDeleteTaskSuccess() {
@@ -137,6 +177,22 @@ func (suite *TaskUsecaseTestSuite) TestDeleteTaskSuccess() {
 
 	assert.NoError(suite.T(), err)
 }	
+
+func (suite *TaskUsecaseTestSuite) TestDeleteTaskError() {
+	taskID := primitive.NewObjectID()
+
+	repositoryErr := errors.New("invalid task ID")
+
+	suite.taskRepository.
+		EXPECT().
+		DeleteTask(taskID).
+		Return(repositoryErr)
+	
+		err := suite.taskUsecase.DeletedTask(taskID)
+
+	assert.Error(suite.T(), err)
+	assert.Equal(suite.T(), err, repositoryErr)
+}
 
 func TestTaskUsecaseTestSuite(t *testing.T) {
 	suite.Run(t, new(TaskUsecaseTestSuite))
